1c317ddccf4aafee76fc919e4c859222e9fddb7a
wangmaolin <wangmaolin12@h-partners.com>
2024-12-17 11:42:28 +0800

    fix: 添加oat、readme 等合规文件

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import SignatureCapture from 'react-native-signature-capture';
 import SignatureCaputreHarmony from './SignatureCapture.harmony';
 import { Platform } from 'react-native';
